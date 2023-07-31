6d1df394af92a3c4d7d126a0b49beac5f577b232
Shehan Rathnayake <shehandj.swd@gmail.com>
2023-08-01 00:13:20 +0530

    Task Q1 completed

---
+++ Q1.java
@@ -0,0 +1,10 @@
+public class Q1 {
+    public static void main (String[] args) {
+
+        float value = 123456.7890f;
+
+        System.out.printf("$%,.2f", value);
+        System.out.println();
+
+    }
+}
