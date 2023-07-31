257312b24dd91521cd7577c775b8330d4a2a2946
Shehan Rathnayake <shehandj.swd@gmail.com>
2023-08-01 01:03:37 +0530

    Task Q2 completed

---
+++ Q2.java
@@ -0,0 +1,8 @@
+public class Q2 {
+    public static void main (String[] args) {
+
+        final double PI = Math.PI;
+        System.out.printf("%.3f",PI);
+        System.out.println();
+    } 
+}
